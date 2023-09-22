e5a3bf8eb68d38ebb78a7899d30b0ba4317af4c8
Saikalyan <saikalyan2406@gmail.com>
2023-09-22 11:29:46 +0530

    context Api tutorial

---
+++ Context_api_tutorial/src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Header from "./components/Header"
+import './App.css'
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    <Header/>
+    <div className="App">
+         <Routes>
+         <Route path="/" element={<Home />}/>
+         <Route path="/cart" element={<Cart />}/>
+         </Routes>
+    </div>
+   </BrowserRouter>   
+  )
+}
+
+export default App
---
+++ Context_api_tutorial/src/components/context/Context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from '@faker-js/faker';
+
+const Cart = createContext();
+faker.seed(100);
+
+const Context = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const productsArray = [...Array(20)].map((p) => ({
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.url(),
+  }));
+
+  const [products] = useState(productsArray);
+
+  return (
+    <Cart.Provider value={{ cart, setCart, products }}>
+      {children}
+    </Cart.Provider>
+  );
+};
+
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
+
