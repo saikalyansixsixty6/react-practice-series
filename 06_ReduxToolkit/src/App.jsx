70300d95735127fe0eb80c5264a558d8e0d11ee5
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-09-28 14:56:59 +0000

    Todos with ReduxToolkit completed

--- 06_ReduxToolkit/src/App.jsx
+++ 06_ReduxToolkit/src/App.jsx
@@ -1,14 +1,14 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
-import './App.css'
 
 function App() {
   
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
