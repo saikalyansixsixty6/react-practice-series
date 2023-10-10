ba8cf79e25a6cb8c675ed2774dfde9daaff69ac9
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-10-10 07:09:00 +0000

    changes

--- 07_handling_APIs/frontend/src/App.jsx
+++ 07_handling_APIs/frontend/src/App.jsx
@@ -1,11 +1,25 @@
 
+import { useState,useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  
+  const[products,setProducts] = useState([])
+
+ useEffect(() => {
+   (async()=>{
+    const response = await axios.get("/api/products")
+    console.log(response.data)
+   })()
+ 
+   
+ }, [])
+ 
+
   return (
     <>
       <h1>Backend</h1>
+      <h2>Number of Products:{products.length}</h2>
         
     </>
   )
--- 07_handling_APIs/frontend/vite.config.js
+++ 07_handling_APIs/frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+      proxy:{
+        "/api":"https://friendly-sniffle-wj46r5v547rhr9-5173.app.github.dev"
+      }
+  },
   plugins: [react()],
 })
